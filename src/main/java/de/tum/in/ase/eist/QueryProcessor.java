2c2608a4145595ce9529a6c044072311fb5a9749
Giulietta Walderdorff <ga92zul@mytum.de>
2022-07-13 13:25:04 +0200

    plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,6 +15,15 @@ public class QueryProcessor {
            return "Giulie";
         } else if (query.contains("what is your name")) {
             return "Giulie";
+        } else if (query.contains("plus")) {
+            String string1 = query.substring(query.indexOf("what is ") + 1);
+            string1 = string1.substring(0, string1.indexOf("plus") - 1);
+            String string2 = query.substring(query.indexOf("plus ") + 1);
+
+            int value1 = Integer.valueOf(string1);
+            int value2 = Integer.valueOf(string2);
+            int result = value1 + value2;
+            return String.valueOf(result);
         } else { // TODO extend the programm here
             return "";
         }
