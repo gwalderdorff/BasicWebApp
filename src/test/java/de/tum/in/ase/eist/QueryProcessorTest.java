5f76376bc6a73dc7d44dccb88de61920ef2ebb3d
Giulietta Walderdorff <ga92zul@mytum.de>
2022-07-13 13:39:23 +0200

    tests

--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -33,15 +33,15 @@ class QueryProcessorTest {
 	@Test
 	void knowsHowToAdd() {
 		String actual = queryProcessor.process("what is 6 plus 19");
-		if (!(actual == "25")) {
+		if (!actual.equals("25")) {
 			fail("Your QueryProcessor is not adding properly.");
-		}//
+		}
 	}
 
 	@Test
 	void knowsHowToIdentifyLargestNumber() {
 		String actual = queryProcessor.process("which of the following is the largest: 0, 10, 100");
-		if (!(actual == "100")) {
+		if (!actual.equals("100")) {
 			fail("Your QueryProcessor is not identifying the largest number properly.");
 		}
 	}
